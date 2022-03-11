cf64f7138a62a130fc7bea287751b315bfd6d5cb
Alexander Horta <alexhortariv@gmail.com>
2022-03-12 01:44:05 +0700

    Typescript1

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import {userRouter} from "./users/users.js";
 
 const port = 8000;
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/users', userRouter);
-app.use( (err, req, res, next) => {
+app.use( (err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(401).send(err.message);
 } );
 
